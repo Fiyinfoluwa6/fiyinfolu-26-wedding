d8be87819a26af50defa27d866e34b0dd78a139c
Favour Oyelami <fiyinfoluwa6@users.noreply.github.com>
2026-09-04 20:43:54 +0000

    Point sheet backend to personal-account Web App URL

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@
  */
 window.WEDDING_CONFIG = {
   // Paste your Google Apps Script Web App URL here:
-  sheetUrl: "https://script.google.com/macros/s/AKfycbwRtSCoDETky3u7IOrpzV-6mJKrYDmLyqQeqrJx3QyRQxdBnNiRDpfFPS-eN8cHReKJQQ/exec",
+  sheetUrl: "https://script.google.com/macros/s/AKfycbzHMwFx404gz3xqqGv1hp_K2MH3MFdBuBa3QZPELo8lJ3B3F6QuWneEt7q5RwdYVmPcUw/exec",
 
   // The image used as the card. Your uploaded flyer:
   imagePath: "assets/invitation.jpeg",
